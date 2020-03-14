dc8c516170bc0e4569a85abeff4bd69b9461d6a5
Daniel Vargas <daniel.vargas@sga.pucminas.br>
2020-03-13 22:03:35 -0300

    Modificação do script para obter os dados necessários

--- GitLab.py
+++ GitLab.py
@@ -4,7 +4,6 @@ import csv
 
 headers = {"Authorization": "token ######"}
 
-initial = "null"
 results = [] #vetor de resultados
 
 def run_query(query):
@@ -18,18 +17,17 @@ def run_query(query):
         raise Exception("Query falhou! Codigo de retorno: {}. {}".format(request.status_code, query))
 
 def createCsv(nodes):
-  with open("/Lab_6/Repositorios.csv", 'w', newline='') as n_file:
+  with open("/Lab_6_EX2/RepositoriosPython.csv", 'w', newline='') as n_file:
 
     fnames = [
         'Nome/Dono;',
         'URL;',
         'Linguagem Primária;',
-        'Pull Requests Aceitas;',
-        'Releases;',
-        'Issues Fechadas;',
-        'Total de Issues;',
-        'Data de Criação;',
-        'Última Atualização;']
+        'Nº de Estrelas;',
+        'Nº de Watchers;',
+        'Nº de Forks;',
+        'Nº de Releases;',
+        'Data de Criação;']
 
     csv_writer = csv.DictWriter(n_file, fieldnames=fnames, dialect="excel-tab")
     csv_writer.writeheader()
@@ -39,59 +37,50 @@ def createCsv(nodes):
                 'Nome/Dono;': "{};".format(node['nameWithOwner']),
                 'URL;': "{};".format(node['url']),
                 'Linguagem Primária;': "{};".format(node['primaryLanguage']['name'] if node['primaryLanguage']!= None else 'null'),
-                'Pull Requests Aceitas;': "{};".format(node['acceptedPullRequests']['totalCount']),
-                'Releases;': "{};".format(node['releases']['totalCount']),
-                'Issues Fechadas;': "{};".format(node['closedIssues']['totalCount']),
-                'Total de Issues;': "{};".format(node['totalIssues']['totalCount']),
-                'Data de Criação;': "{};".format(node['createdAt']),
-                'Última Atualização;': "{};".format(node['updatedAt'])
+                'Nº de Estrelas;': "{};".format(node['stargazers']['totalCount']),
+                'Nº de Watchers;': "{};".format(node['watchers']['totalCount']),
+                'Nº de Forks;': "{};".format(node['forkCount']),
+                'Nº de Releases;': "{};".format(node['releases']['totalCount']),
+                'Data de Criação;': "{};".format(node['createdAt'])
             })
-
-for x in range(50):        
-      query = """
-      {
-        search(query:"stars:>100", type:REPOSITORY, first:20, after:%s){
-          nodes{
-            ... on Repository
-            {
-              nameWithOwner
-              url
-              primaryLanguage
-              {
-                name
-              }
-              acceptedPullRequests: pullRequests(states:MERGED)
-              {
-                totalCount
-              }
-              releases
-              {
-                totalCount
-              }
-              closedIssues: issues(states:CLOSED)
-              {
-                totalCount
-              }
-              totalIssues: issues
-              {
-                totalCount
-              }
-              createdAt
-              updatedAt
-            }
-          }
-          pageInfo
+   
+query = """
+{
+  user(login:"gvanrossum"){
+    repositories(first:39){
+      nodes{
+        ... on Repository
+        {
+          nameWithOwner
+          url
+          primaryLanguage
           {
-            endCursor
+            name
           }
+          stargazers
+          {
+            totalCount
+          }
+          watchers
+          {
+            totalCount
+          }
+          forkCount
+          releases
+          {
+            totalCount
+          }
+          createdAt
         }
       }
-      """ % (initial)
+    }
+  }
+}
+"""
 
-      result = run_query(query)
-      for y in range(20):
-        results.append(result["data"]["search"]["nodes"][y])
-      initial = '"{}"'.format(result["data"]["search"]["pageInfo"]["endCursor"])
+result = run_query(query)
+for y in range(39):
+  results.append(result["data"]["user"]["repositories"]["nodes"][y])
 
 print('Dados obtidos com sucesso')
 createCsv(results)
